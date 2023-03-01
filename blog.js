f7103e34611f86411898dd428f344ebd1504f5c1
JGong7 <97704595+JGong7@users.noreply.github.com>
2023-03-01 14:33:58 -0800

    deleet

--- blog.js
+++ blog.js
@@ -1,20 +1,35 @@
-export function diashow(){
+let but = document.getElementById('but');
+but.addEventListener("click", diashow);
+let cancel = document.getElementById('prompt-no');
+cancel.addEventListener("click", dianoshow);
+let ok = document.getElementById('prompt-yes');
+ok.addEventListener("click", addtolist);
+function diashow(){
     let dia = document.getElementById("add");
     dia.show();
 }
 
-export function dianoshow(){
+function dianoshow(){
     let dia = document.getElementById("add");
     dia.close();
 }
 
-export function addtolist(){
+function deleteitem(del){
+    let item = del.parentElement;
+    let list = del.parentElement.parentElement;
+    list.removeChild(item);
+}
+
+
+function addtolist(){
+    let dia = document.getElementById("add");
+    dia.close();
     let title = document.getElementById("title").value;
     let date = document.getElementById("date").value;
     let summary = document.getElementById("summary").value;
-    let list = document.getElementsById("list");
+    let list = document.getElementById("list");
     let post = document.createElement('li');
-    post.appendChild(document.createTextNode(title));
+    post.innerHTML = title + " (" + date + ") " + "Summary: " + summary + "  <b id='edit' onClick='deleteitem(this)'>Edit<b>  <b id='edit'>Delete<b>";
     list.appendChild(post);
-    
 }
+
