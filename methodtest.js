120622b829565d00c8497877a539823ec47be621
JGong7 <97704595+JGong7@users.noreply.github.com>
2023-03-15 19:50:54 -0700

    2

--- methodtest.js
+++ methodtest.js
@@ -9,7 +9,16 @@ function clear(){
     let field = document.getElementById("date");
     field.value = new Date();
 }
-function post(){
+function postCheck(){
+    let xml = document.getElementById('xml');
+    let fetch = document.getElementById('fetch');
+    if (xml.checked){
+        XMLpost();
+    }else if(fetch.checked){
+        fetchpost();
+    }
+}
+function XMLpost(){
     let request = new XMLHttpRequest();
     request.open("POST", "https://httpbin.org/post");
     let iid = document.getElementById("id").value;
@@ -30,7 +39,35 @@ function post(){
         res.innerText = JSON.stringify(request.response, null, 2);
     })
 }
-function get(){
+function fetchpost(){
+    let request = new FormData();
+    let iid = document.getElementById("id").value;
+    let name = document.getElementById("article_name").value;
+    let body = document.getElementById("article_body").value;
+    let writtendate = document.getElementById("date").value;
+    request.set("id", iid);
+    request.set("article_name", name);
+    request.set("article_body", body);
+    request.set("date", writtendate);
+
+    fetch("https://httpbin.org/post", {method: "POST", body: request})
+    .then(response => response.json())
+    .then(data =>{
+        let res = document.getElementById("response");
+        res.innerText = JSON.stringify(data, null, 2);
+        clear();
+    })
+}
+function getCheck(){
+    let xml = document.getElementById('xml');
+    let fetch = document.getElementById('fetch');
+    if (xml.checked){
+        XMLget();
+    }else if(fetch.checked){
+        fetchget();
+    }
+}
+function XMLget(){
     let request = new XMLHttpRequest();
     let iid = document.getElementById("id").value;
     let url;
@@ -48,7 +85,28 @@ function get(){
         res.innerText = JSON.stringify(request.response, null, 2);
     })
 }
-function put(){
+function fetchget(){
+    let iid = document.getElementById("id").value;
+    let url = `https://httpbin.org/get?id=${iid}`;
+
+    fetch(url, {method: "GET"})
+    .then(response => response.json())
+    .then(data =>{
+        let res = document.getElementById("response");
+        res.innerText = JSON.stringify(data, null, 2);
+        clear();
+    })
+}
+function putCheck(){
+    let xml = document.getElementById('xml');
+    let fetch = document.getElementById('fetch');
+    if (xml.checked){
+        XMLput();
+    }else if(fetch.checked){
+        fetchput();
+    }
+}
+function XMLput(){
     let request = new XMLHttpRequest();
     request.open("PUT", "https://httpbin.org/put");
     let iid = document.getElementById("id").value;
@@ -69,7 +127,35 @@ function put(){
         res.innerText = JSON.stringify(request.response, null, 2);
     })
 }
-function del(){
+function fetchput(){
+    let request = new FormData();
+    let iid = document.getElementById("id").value;
+    let name = document.getElementById("article_name").value;
+    let body = document.getElementById("article_body").value;
+    let writtendate = document.getElementById("date").value;
+    request.set("id", iid);
+    request.set("article_name", name);
+    request.set("article_body", body);
+    request.set("date", writtendate);
+
+    fetch("https://httpbin.org/put", {method: "PUT", body: request})
+    .then(response => response.json())
+    .then(data =>{
+        let res = document.getElementById("response");
+        res.innerText = JSON.stringify(data, null, 2);
+        clear();
+    })
+}
+function delCheck(){
+    let xml = document.getElementById('xml');
+    let fetch = document.getElementById('fetch');
+    if (xml.checked){
+        XMLdel();
+    }else if(fetch.checked){
+        fetchdel();
+    }
+}
+function XMLdel(){
     let request = new XMLHttpRequest();
     let iid = document.getElementById("id").value;
     let url;
@@ -85,5 +171,18 @@ function del(){
         clear();
         let res = document.getElementById("response");
         res.innerText = JSON.stringify(request.response, null, 2);
+    })
+}
+
+function fetchdel(){
+    let iid = document.getElementById("id").value;
+    let url = `https://httpbin.org/delete?id=${iid}`;
+
+    fetch(url, {method: "DELETE"})
+    .then(response => response.json())
+    .then(data =>{
+        let res = document.getElementById("response");
+        res.innerText = JSON.stringify(data, null, 2);
+        clear();
     })
 }
